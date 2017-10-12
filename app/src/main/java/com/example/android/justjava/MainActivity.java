bd6cb2202c1c6e02d97bc518042efd337c4f0559
Samir Gandhi <samirgandhi19@gmail.com>
2017-10-11 19:14:47 -0500

    Initial commit, completed Interactive App - lesson 2

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,93 @@
+/**
+ * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ *
+ * package com.example.android.justjava;
+ *
+ */
+package com.example.android.justjava;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    int quantity = 0;
+    int pricePerCup = 5;
+    String customerName = "Samir";
+    public void submitOrder(View view) {
+        String priceMessage;
+
+        if (quantity == 0) {
+            displayMessage("Nothing Ordered!");
+        }
+        else {
+            displayMessage(createOrderSummary(customerName, quantity, calculatePrice(quantity,pricePerCup)));
+        }
+    }
+
+    public void goToLogin(View view) {
+        String priceMessage;
+
+        if (quantity == 0) {
+            displayMessage("Nothing Ordered!");
+        }
+        else {
+            displayMessage(createOrderSummary(customerName, quantity, calculatePrice(quantity,pricePerCup)));
+        }
+    }
+
+    public void increment(View view) {
+        quantity++;
+        displayQuantity(quantity);
+    }
+    public void decrement(View view) {
+        quantity--;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method calculates the price by multiplying pricePerCup and Quantity.
+     */
+    private int calculatePrice(int quantity, int pricePerCup) {
+        return (quantity * pricePerCup);
+    }
+
+    private String createOrderSummary (String customerName, int qty, int total) {
+        String msg = ("Name: " + customerName + "\nQuantity: " + qty + "\nTotal: " + NumberFormat.getCurrencyInstance().format(total) + "\n Thank you!");
+        return msg;
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+}
